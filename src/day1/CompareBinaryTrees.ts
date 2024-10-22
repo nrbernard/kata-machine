398e8427094b7e8b65a1c9a3fd91eeaedd678e8e
Nick Bernard <nick.bernard@opensesame.com>
2024-10-22 15:34:23 -0700

    Implement binary tree comparison

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,18 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    // structural check
+    if (a === null && b === null) {
+        return true;
+    }
+    // structural check
+    if (a === null || b === null) {
+        return false;
+    }
+    // value check
+    if (a.value !== b.value) {
+        return false;
+    }
 
+    // when we compare any two sub-trees, as long as both return true
+    // then they are equal
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
